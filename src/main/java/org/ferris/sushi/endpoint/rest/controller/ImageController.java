6b0ddf3d141ff24318747853091cc5ec053c41d8
NickyHariniaina <ferrissushi@gmail.com>
2026-07-13 08:52:19 +0300

    feat: update ImageController with POST/GET endpoints

--- src/main/java/org/ferris/sushi/endpoint/rest/controller/ImageController.java
+++ src/main/java/org/ferris/sushi/endpoint/rest/controller/ImageController.java
@@ -1,26 +1,34 @@
 package org.ferris.sushi.endpoint.rest.controller;
 
+import java.io.IOException;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import org.ferris.sushi.endpoint.event.EventProducer;
-import org.ferris.sushi.endpoint.event.model.SendEmailRequested;
+import org.ferris.sushi.repository.model.ImageSubmission;
+import org.ferris.sushi.service.ImageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @AllArgsConstructor
 public class ImageController {
 
-  private final EventProducer<SendEmailRequested> eventProducer;
+  private final ImageService imageService;
 
   @PostMapping("/images")
-  public ResponseEntity<String> submit(@RequestParam String email) {
-    var event =
-        SendEmailRequested.builder().to(email).body("Your image has been received.").build();
-    eventProducer.accept(List.of(event));
-    return ResponseEntity.status(HttpStatus.CREATED).body("ok");
+  public ResponseEntity<ImageSubmission> submit(
+      @RequestParam("file") MultipartFile file, @RequestParam("email") String email)
+      throws IOException {
+    ImageSubmission submission = imageService.submitImage(file, email);
+    return ResponseEntity.status(HttpStatus.CREATED).body(submission);
+  }
+
+  @GetMapping("/images")
+  public List<ImageSubmission> findAll() {
+    return imageService.findAll();
   }
 }
