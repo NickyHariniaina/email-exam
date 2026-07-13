8a5656b85c6a0b5c33777f169dc46714d03f3bfc
NickyHariniaina <ferrissushi@gmail.com>
2026-07-13 08:21:21 +0300

    feat: add SendEmailRequestedService event consumer

---
+++ src/main/java/org/ferris/sushi/service/event/SendEmailRequestedService.java
@@ -0,0 +1,28 @@
+package org.ferris.sushi.service.event;
+
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
+import java.util.function.Consumer;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.ferris.sushi.PojaGenerated;
+import org.ferris.sushi.endpoint.event.model.SendEmailRequested;
+import org.ferris.sushi.mail.Email;
+import org.ferris.sushi.mail.Mailer;
+import org.springframework.stereotype.Service;
+
+@PojaGenerated
+@Service
+@AllArgsConstructor
+public class SendEmailRequestedService implements Consumer<SendEmailRequested> {
+  private final Mailer mailer;
+
+  @SneakyThrows
+  @Override
+  public void accept(SendEmailRequested sendEmailRequested) {
+    InternetAddress recipientAddress = new InternetAddress(sendEmailRequested.getTo());
+    String body = sendEmailRequested.getBody();
+    mailer.accept(
+        new Email(recipientAddress, List.of(), List.of(), "REGISTER SUCCESSFUL", body, List.of()));
+  }
+}
