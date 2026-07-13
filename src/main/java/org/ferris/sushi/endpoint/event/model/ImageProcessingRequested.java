577342f98f904a1703d4b44d35a67b59e95b6fc6
NickyHariniaina <ferrissushi@gmail.com>
2026-07-13 08:49:48 +0300

    feat: add ImageProcessingRequested event model

---
+++ src/main/java/org/ferris/sushi/endpoint/event/model/ImageProcessingRequested.java
@@ -0,0 +1,37 @@
+package org.ferris.sushi.endpoint.event.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.Duration;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class ImageProcessingRequested extends PojaEvent {
+  @JsonProperty("submission_id")
+  private String submissionId;
+
+  @JsonProperty("email")
+  private String email;
+
+  @JsonProperty("s3_key")
+  private String s3Key;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(60);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofSeconds(30);
+  }
+}
